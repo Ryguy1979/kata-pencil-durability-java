ec0a46839a4ea948415c86f68aac6803aee713ca
Rylan T VW <rylantvw@gmail.com>
2018-11-04 20:43:40 -0600

    Eraser durability tests.

--- src/main/java/Eraser.java
+++ src/main/java/Eraser.java
@@ -1,7 +1,31 @@
 class Eraser {
-    void erase(String wordToErase, Paper paper) {
+
+    private static final int ERASE_CHARACTER_VALUE = 1;
+    private static final int WHITESPACE_ERASE_VALUE = 0;
+
+    private static final int ERASER_DEFAULT_DURABILITY = 40000;
+
+    Eraser() {
+        this.eraserDurability = ERASER_DEFAULT_DURABILITY;
+    }
+
+    int getEraserDurability() {
+        return eraserDurability;
+    }
+
+    void setEraserDurability(int eraserDurability) {
+        this.eraserDurability = eraserDurability;
+    }
+
+    private int eraserDurability;
+
+    void erase(String wordToErase, Paper paper) throws Exception {
         String stringToModify = paper.getText();
         int startReplacement = stringToModify.lastIndexOf(wordToErase);
+
+        if (startReplacement == -1) {
+            throw new Exception("Word to replace not found");
+        }
 
         StringBuilder builder = new StringBuilder();
 
@@ -14,9 +38,17 @@ class Eraser {
         char[] returnedChars = wordToErase.toCharArray();
 
         for (int i = wordToErase.length() - 1; i > -1; i--) {
-            returnedChars[i] = ' ';
+            if (eraserDurability >= calculateCharacterCost((char) wordToErase.indexOf(i))) {
+                returnedChars[i] = ' ';
+            }
+
+            eraserDurability -= calculateCharacterCost((char) wordToErase.indexOf(i));
         }
 
         return returnedChars;
+    }
+
+    private int calculateCharacterCost(Character writtenCharacter) {
+        return Character.isWhitespace(writtenCharacter) ? WHITESPACE_ERASE_VALUE : ERASE_CHARACTER_VALUE;
     }
 }
--- src/test/java/EraserTest.java
+++ src/test/java/EraserTest.java
@@ -2,6 +2,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class EraserTest {
 
@@ -15,7 +16,7 @@ class EraserTest {
     }
 
     @Test
-    void eraserErasesGivenTextFromPaper() {
+    void eraserErasesGivenTextFromPaper() throws Exception {
         paper.setText("How much wood would a woodchuck chuck if a woodchuck could chuck wood?");
 
         eraser.erase("chuck", paper);
@@ -24,12 +25,51 @@ class EraserTest {
     }
 
     @Test
-    void eraserErasesGivenTextFromPaperTwice() {
+    void eraserErasesGivenTextFromPaperTwice() throws Exception {
         paper.setText("How much wood would a woodchuck chuck if a woodchuck could chuck wood?");
 
         eraser.erase("chuck", paper);
         eraser.erase("chuck", paper);
 
         assertEquals("How much wood would a woodchuck chuck if a wood      could       wood?", paper.getText());
+    }
+
+    @Test
+    void eraserStartsWithDefaultDurability() {
+        assertEquals(40000, eraser.getEraserDurability());
+    }
+
+    @Test
+    void eraserThrowsExceptionWhenWordToEraseNotFound() {
+        paper.setText("Buffalo Bil");
+
+        assertThrows(Exception.class, () -> eraser.erase("Bill", paper));
+    }
+
+    @Test
+    void eraserErasesBillWithEnoughDurability() throws Exception {
+        eraser.setEraserDurability(10);
+        paper.setText("Buffalo Bill");
+        eraser.erase("Bill", paper);
+
+        assertEquals("Buffalo     ", paper.getText());
+    }
+
+    @Test
+    void eraserWithThreeDurabilityLeavesB() throws Exception {
+        eraser.setEraserDurability(3);
+        paper.setText("Buffalo Bill");
+        eraser.erase("Bill", paper);
+
+        assertEquals("Buffalo B   ", paper.getText());
+    }
+
+    @Test
+    void eraserWithTwoDurabilityLeavesBi() throws Exception {
+        eraser.setEraserDurability(2);
+        paper.setText("Buffalo Bill");
+        eraser.erase("Bill", paper);
+
+        assertEquals("Buffalo Bi  ", paper.getText());
     }
 }
